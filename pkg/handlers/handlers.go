2a0798006eba6c875d966085a49e7ad2f3ff1943
atomiksan <nsofbl@gmail.com>
2024-10-01 23:22:31 +0530

    added the app config to handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,35 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/atomiksan/webapp-in-go/pkg/config"
 	"github.com/atomiksan/webapp-in-go/pkg/render"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the epository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the homepage handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
