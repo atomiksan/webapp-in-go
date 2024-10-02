5f70fa603ce7b90517b89c84021c3a0352c15f48
atomiksan <nsofbl@gmail.com>
2024-10-02 15:50:55 +0530

    modified handlers to take provided template data struct

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/atomiksan/webapp-in-go/pkg/config"
+	"github.com/atomiksan/webapp-in-go/pkg/models"
 	"github.com/atomiksan/webapp-in-go/pkg/render"
 )
 
@@ -28,10 +29,18 @@ func NewHandlers(r *Repository) {
 
 // Home is the homepage handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some business logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
 }
